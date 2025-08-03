47ec66154873f307df9706c5d727744954dec923
Ilo Tech <ilo.nikatsadze@gmail.com>
2025-08-03 21:27:36 +0400

    models modified

--- server/models/db.js
+++ server/models/db.js
@@ -1,20 +1,23 @@
-const mongoose=require("mongoose");
+import mongoose from "mongoose";
 
-const subSchema=new mongoose.Schema({
-    display_text:String,
-    position:Number
-})
+const subSchema = new mongoose.Schema(
+  {
+    display_text: String,
+    position: Number,
+  },
+  { _id: false }
+);
 
-const schema=new mongoose.Schema({
-   aspect_ratio:String,
-   name:String,
-   id:Number,
-   thumbnail_url:String,
-   description:String,
-   instructions:[subSchema]
-})
+const schema = new mongoose.Schema({
+  aspect_ratio: String,
+  name: String,
+  id: Number,
+  thumbnail_url: String,
+  description: String,
+  instructions: [subSchema],
+});
 
-const Recipes=mongoose.model("recipes",schema);
+const Recipes = mongoose.model("recipes", schema);
+const Favourites = mongoose.model("favourites", schema);
 
-module.exports=Recipes
-
+export { Recipes, Favourites };
